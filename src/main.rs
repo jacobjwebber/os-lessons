94db5e1ff293f72df15cfbe7cf004833775ed7d4
jjw <jacobjwebber@gmail.com>
2020-03-29 18:31:24 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_stuff();
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
